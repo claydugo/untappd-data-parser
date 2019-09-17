2d928c9762b2f01980285246ded5b8090e6137ff
Clay Dugo <claydugo@gmail.com>
2019-09-17 13:35:11 -0400

    pep8 and formatting fixes

--- untappd.py
+++ untappd.py
@@ -10,17 +10,39 @@ a = p.parse_args()
 data = json.load(open(a.file))
 fne = str(a.file[:len(a.file)-5]) + '-uniques.csv'
 fnej = str(a.file[:len(a.file)-5]) + '-uniques.json'
-csv_header = ['beer_name', 'brewery_name', 'beer_type', 'beer_abv', 'beer_ibu', 'comment', 'venue_name',
-                     'venue_city', 'venue_state', 'venue_country', 'venue_lat', 'venue_lng', 'rating_score',
-                     'created_at', 'checkin_url', 'beer_url', 'brewery_url', 'brewery_country', 'brewery_city',
-                     'brewery_state', 'flavor_profiles', 'purchase_venue', 'serving_type']
+csv_header = [
+    'beer_name',
+    'brewery_name',
+    'beer_type',
+    'beer_abv',
+    'beer_ibu',
+    'comment',
+    'venue_name',
+    'venue_city',
+    'venue_state',
+    'venue_country',
+    'venue_lat',
+    'venue_lng',
+    'rating_score',
+    'created_at',
+    'checkin_url',
+    'beer_url',
+    'brewery_url',
+    'brewery_country',
+    'brewery_city',
+    'brewery_state',
+    'flavor_profiles',
+    'purchase_venue',
+    'serving_type',
+    ]
+
 
 def main():
-    uniques = list({x['beer_url']:x for x in data}.values())
+    uniques = list({x['beer_url']: x for x in data}.values())
     dupes = len(data) - len(uniques)
 
-    print("You have " + str(len(data)) + ' total check-ins with ' + str(len(uniques)) + ' uniques and ' +
-          str(dupes) + ' duplicates')
+    print("You have " + str(len(data)) + ' total check-ins with ' +
+          str(len(uniques)) + ' uniques and ' + str(dupes) + ' duplicates')
 
     with open(fne, 'w') as fcsv:
         cw = csv.DictWriter(fcsv, fieldnames=csv_header)
@@ -30,6 +52,7 @@ def main():
     with open(fnej, 'w') as fjson:
         json.dump(uniques, fjson)
 
-    print('\nCreated ' + fne + ' and ' + fnej + ' with just your unique beer check-ins')
+    print('\nCreated ' + fne + ' and ' + fnej +
+          ' with just your unique beer check-ins')
 
 main()
