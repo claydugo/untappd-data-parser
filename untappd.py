16eee6b514782d0b25b298df1bd6862bdccf6a76
Clay Dugo <claydugo@gmail.com>
2019-09-18 09:12:32 -0400

    cleaned up, optimized

--- untappd.py
+++ untappd.py
@@ -10,56 +10,23 @@ a = p.parse_args()
 data = json.load(open(a.file))
 fne = str(a.file[:len(a.file)-5]) + '-uniques.csv'
 fnej = str(a.file[:len(a.file)-5]) + '-uniques.json'
-csv_header = [
-    'beer_name',
-    'brewery_name',
-    'beer_type',
-    'beer_abv',
-    'beer_ibu',
-    'comment',
-    'venue_name',
-    'venue_city',
-    'venue_state',
-    'venue_country',
-    'venue_lat',
-    'venue_lng',
-    'rating_score',
-    'created_at',
-    'checkin_url',
-    'beer_url',
-    'brewery_url',
-    'brewery_country',
-    'brewery_city',
-    'brewery_state',
-    'flavor_profiles',
-    'purchase_venue',
-    'serving_type',
-    'checkin_id',
-    'bid',
-    'brewery_id',
-    'photo_url',
-    'global_rating_score',
-    'global_weighted_rating_score'
-    ]
 
 
-def main():
-    uniques = list({x['beer_url']: x for x in data}.values())
-    dupes = len(data) - len(uniques)
+uniques = list({x['beer_url']: x for x in data}.values())
+csv_header = list(data[0].keys())
+dupes = len(data) - len(uniques)
 
-    print("You have " + str(len(data)) + ' total check-ins with ' +
-          str(len(uniques)) + ' uniques and ' + str(dupes) + ' duplicates')
+print("You have " + str(len(data)) + ' total check-ins with ' +
+      str(len(uniques)) + ' uniques and ' + str(dupes) + ' duplicates\n')
 
-    with open(fne, 'w') as fcsv:
-        cw = csv.DictWriter(fcsv, fieldnames=csv_header)
-        cw.writeheader()
-        for i in range(0, len(uniques)):
-            cw.writerow(uniques[i])
+with open(fne, 'w') as fcsv:
+    cw = csv.DictWriter(fcsv, fieldnames=csv_header)
+    cw.writeheader()
+    for i in range(0, len(uniques)):
+        cw.writerow(uniques[i])
 
-    with open(fnej, 'w') as fjson:
-        json.dump(uniques, fjson, separators=(',', ':'))
+with open(fnej, 'w') as fjson:
+    json.dump(uniques, fjson, separators=(',', ':'))
 
-    print('\nCreated ' + fne + ' and ' + fnej +
-          ' with just your unique beer check-ins')
-
-main()
+print('Created ' + fne + ' and ' + fnej +
+      ' with just your unique beer check-ins')
