6101028c33e8c6d9046700cbaf538f2050453cda
Clay Dugo <claydugo@gmail.com>
2022-10-02 16:04:07 -0400

    Better variables and organization

--- untappd.py
+++ untappd.py
@@ -2,25 +2,25 @@
 import json, csv, argparse, os
 
 
-def parseData(data, key):
-    uniques = list({x[key]: x for x in data}.values())
-    dupes = len(data) - len(uniques)
+def parse_beer_data(beer_data, key):
+    unique_beers = list({x[key]: x for x in beer_data}.values())
+    duplicate_beers = len(beer_data) - len(unique_beers)
 
-    print(f'You have {len(data)} total check-ins with {len(uniques)} unique {key}\'s '
-          f'and {dupes} duplicates\n')
+    print(f'You have {len(beer_data)} total check-ins with {len(unique_beers)} unique {key}\'s '
+          f'and {duplicate_beers} duplicates\n')
 
-    return uniques
+    return unique_beers
 
-def makeFiles(filename, data):
-    csv_header = list(data[0].keys())  # Get headers from file
-    with open(f'{filename}-uniques.csv', 'w') as fcsv:
-        cw = csv.DictWriter(fcsv, fieldnames=csv_header)
-        cw.writeheader()
-        for i in range(0, len(data)):
-            cw.writerow(data[i])
+def make_untappd_files(filename, beer_data):
+    csv_header = list(beer_data[0].keys())  # Get headers from file
+    with open(f'{filename}.csv', 'w') as fcsv:
+        csv_writer = csv.DictWriter(fcsv, fieldnames=csv_header)
+        csv_writer.writeheader()
+        for check_in in range(0, len(beer_data)):
+            csv_writer.writerow(beer_data[check_in])
 
-    with open(f'{filename}-uniques.json', 'w') as fjson:
-        json.dump(data, fjson, separators=(',', ':'))
+    with open(f'{filename}.json', 'w') as fjson:
+        json.dump(beer_data, fjson, separators=(',', ':'))
 
     print('Created JSON and CSV with just your unique'
       ' check-ins')
@@ -29,16 +29,16 @@ def makeFiles(filename, data):
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--key', choices=['brewery_name', 'venue_name', 'beer_type', 'photo_url'])
-    p.add_argument('file', help='name of the json file to extract uniques from')
-    a = p.parse_args()
+    p.add_argument('file', help='name of the json file to extract unique_beers from')
+    arguments = p.parse_args()
 
-    if a.key:
-        key = a.key
+    if arguments.key:
+        key = arguments.key
     else:
         key = 'bid' # If no key specified sort by unique beers
 
-    data = json.load(open(a.file))
-    fn = os.path.splitext(a.file)[0]
+    beer_data = json.load(open(arguments.file))
+    file_name = os.path.splitext(arguments.file)[0] + f'_unique_{key}'
 
-    makeFiles(fn, parseData(data, key))
+    make_untappd_files(file_name, parse_beer_data(beer_data, key))
 
