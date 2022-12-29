55cd275c7dc332f3b92aa4d530f5de2600888289
Clay Dugo <claydugo@gmail.com>
2022-12-29 16:46:36 -0500

    Add formatting functions

--- README.md
+++ README.md
@@ -3,14 +3,11 @@
 
 [Untappd](https://untappd.com/) allows you to download your checkin data in JSON and CSV formats (if you are a [supporter](https://untappd.com/supporter)). This is great, however they do not have an option to download the data of just your 'unique' checkins. This script will take the json file you downloaded from untappd and create both json and csv files with only your last checkins of each beer.
 
-#### Why?
-For unique beers, I have personally checked in my favorite beer over 50 times with a rating of 5 stars and this skews my overall average ratings if using the standard dataset from untappd. (On your profile they calculate average from uniques only)
-
-For unique venues, I use the service Google MyMaps to create the [beer map](https://claydugo.com/beermap/) on my website and I've found it has a cap of 2000 rows for uploaded csv data. While I will have to revisit this issue once I reach 2000 venues, this solution is currently fine for the foreseeable future.
+There are also additional parsing options detailed below. I use this to generate the [beer map](https://claydugo.com/beermap/) on my website. So this has become mostly tailored towards that.
 
 ## Usage
 
-###### To sort by unique beers using their BeerID (bid)  
+###### To sort by unique beers using their BeerID (bid)
 Run `python3 untappd.py <UNTAPPD-DATA>.json`
 
 ###### To sort by a different key
@@ -23,6 +20,20 @@ Adding additional keys will be trivial for you, but these made the most sense to
 Run `python3 untappd.py <UNTAPPD-DATA>.json --key <key_name>`
 
 ![output](scr/untappd-uniques-cl.png)
+
+##### Additional Flags
+
+`--no_human_keys` will stop the key conversions
+
+E.g. `venue_name -> Venue Name`
+
+
+`--no_strip_backend` will keep all keys included in the json file
+
+I current only keep the ones at the top of `untappd.py` by default.
+
+
+`--no_fancy_data_format` will keep the `created_at` key in the `2022-01-01 23:59:59` format.
 
 ## License
 MIT
--- untappd.py
+++ untappd.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
-import json, csv, argparse, os
+import json
+import csv
+import argparse
+import os
+from datetime import datetime
+
+# This is obviously subjective
+# I'm most concerned with not showcasing
+# how the sausage is made
+# So I just specify the few that I actually want
+good_keys = [
+    'beer_name',
+	'brewery_name',
+	'beer_type',
+	'venue_name',
+	'venue_lat',
+	'venue_lng',
+	'created_at',
+]
 
 
 def parse_beer_data(beer_data, key):
-    unique_beers = list({x[key]: x for x in beer_data}.values())
-    duplicate_beers = len(beer_data) - len(unique_beers)
+    return list({x[key]: x for x in beer_data}.values())
 
-    print(f'You have {len(beer_data)} total check-ins with {len(unique_beers)} unique {key}\'s '
-          f'and {duplicate_beers} duplicates\n')
+def purge_backend_keys(beer_json, backend_keys):
+    return [dict((k,v) for k,v in check_in.items() if k not in backend_keys) for check_in in beer_json]
 
-    return unique_beers
+def string_parse_keys(beer_json):
+    return [dict((k.replace('_', ' ').title(), v) for k,v in check_in.items()) for check_in in beer_json]
+
+def fancy_date_format(beer_json):
+    for check_in in beer_json:
+        date = check_in.pop('created_at', None)
+        if date is not None:
+            date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').strftime('%B %d, %Y at %I:%M%p')
+        check_in['Date Drank'] =  date
+    return beer_json
 
 def make_untappd_files(filename, beer_data):
-    csv_header = list(beer_data[0].keys())  # Get headers from file
+    csv_header = list(beer_data[-1].keys())  # Get headers from file
     with open(f'{filename}.csv', 'w') as fcsv:
         csv_writer = csv.DictWriter(fcsv, fieldnames=csv_header)
         csv_writer.writeheader()
@@ -23,12 +49,14 @@ def make_untappd_files(filename, beer_data):
         json.dump(beer_data, fjson, separators=(',', ':'))
 
     print('Created JSON and CSV with just your unique'
-      ' check-ins')
-
+          ' check-ins')
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--key', choices=['brewery_name', 'venue_name', 'beer_type', 'photo_url'])
+    p.add_argument('--no_human_keys', action=argparse.BooleanOptionalAction)
+    p.add_argument('--no_strip_backend', action=argparse.BooleanOptionalAction)
+    p.add_argument('--no_fancy_date_format', action=argparse.BooleanOptionalAction)
     p.add_argument('file', help='name of the json file to extract unique_beers from')
     arguments = p.parse_args()
 
@@ -40,5 +68,21 @@ if __name__ == '__main__':
     beer_data = json.load(open(arguments.file))
     file_name = os.path.splitext(arguments.file)[0] + f'_unique_{key}'
 
-    make_untappd_files(file_name, parse_beer_data(beer_data, key))
+    unique_beers = parse_beer_data(beer_data, key)
+
+    if not arguments.no_strip_backend:
+        backend_keys = [key for key in beer_data[-1].keys() if key not in good_keys]
+        unique_beers = purge_backend_keys(unique_beers, backend_keys)
+
+    if not arguments.no_fancy_date_format:
+        unique_beers = fancy_date_format(unique_beers)
+
+    if not arguments.no_human_keys:
+        unique_beers = string_parse_keys(unique_beers)
+
+    make_untappd_files(file_name, unique_beers)
+    duplicate_beers = len(beer_data) - len(unique_beers)
+
+    print(f'You have {len(beer_data)} total check-ins with {len(unique_beers)} unique {key}\'s '
+          f'and {duplicate_beers} duplicates\n')
 
